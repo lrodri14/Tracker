2a37c264a9386d7cad623553dc009ed7d7e1d341
pmchndev <mbaquedano@promacohn.com>
2021-06-02 17:05:56 -0600

    Corrección en fecha final de planilla generada en febrero

--- employee/worksheet/views.py
+++ employee/worksheet/views.py
@@ -13717,8 +13717,9 @@ def planilla_generar_calculos(request):
                                             #Se suma a resultado de dias trabajados el restante entre la fecha final de la relación profesional y
                                             #la fecha de inicio del contrato
                                             res_dia_trab += (v_fecha_final - contrato.fecha_inicio).days + 1
-                                            if res_dia_trab < total_dias_trabajados:
-                                                res_dia_trab = total_dias_trabajados
+                                            if not item.termDate:
+                                                if res_dia_trab < total_dias_trabajados:
+                                                    res_dia_trab = total_dias_trabajados
                                             
                                         #Se evalúa si la fecha inicio de contrato es antes que la fecha inicio de planilla y la fecha final de
                                         #relación profesional está entre el rango de fecha de inicio y final de planilla
@@ -13726,8 +13727,9 @@ def planilla_generar_calculos(request):
                                             #Se suma a resultado de dias trabajados el restante entre la fecha final de la relación profesional y
                                             #la fecha de incio de la planilla
                                             res_dia_trab += (v_fecha_final - o_planilla.fecha_inicio).days + 1
-                                            if res_dia_trab < total_dias_trabajados:
-                                                res_dia_trab = total_dias_trabajados
+                                            if not item.termDate:
+                                                if res_dia_trab < total_dias_trabajados:
+                                                    res_dia_trab = total_dias_trabajados
 
                                         #Se evalúa si la fecha final de relación profesional es después de la fecha final de la planilla y
                                         #la fecha inicial del contrato es entre el rango de fecha inicial y final de la planilla
